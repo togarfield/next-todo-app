24192411686b62df3aa804edba1e5eb4022c05d8
togarfield <mabduhsiregar@gmail.com>
2025-07-13 14:35:15 +0700

    add file utils and install missing package

---
+++ lib/utils.ts
@@ -0,0 +1,14 @@
+import { type ClassValue, clsx} from 'clsx';
+import { twMerge } from 'tailwind-merge'
+
+export function cn(...inputs: ClassValue[]) {
+    return twMerge(clsx(inputs))
+}
+
+export function formDate(date: Date) {
+    return new Intl.DateTimeFormat('en-US', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+    }).format(date)
+}
